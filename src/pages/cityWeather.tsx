1a84965927c6cd64aaa727b0ea12d5e4573a0b2d
Elena Kroupkin <pocoat@gmail.com>
2023-11-21 00:23:31 +1300

    refacturing cocktail data. now renders only ones

--- src/pages/cityWeather.tsx
+++ src/pages/cityWeather.tsx
@@ -2,15 +2,11 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Header from './components/Header'
-// import { CocktailData } from '~/utils/cocktailTypes'
 import ModalRecipe from './components/ModalRecipe'
 import { Transition } from '@headlessui/react'
 import { Fragment } from 'react'
-import modalIMG from '/public/modalIMG6.png'
 import styles from '../styles/Home.module.css'
-// import wf from '/public/wf.png'
 import ModalWeather from './components/ModalWeather'
-// import { WeatherData, WeatherDataResponse } from '~/utils/weatherTypes'
 import { set } from 'mongoose'
 
 import { useRouter } from 'next/router'
@@ -20,89 +16,85 @@ import { CocktailData } from '~/utils/cocktailTypes'
 const CityWeather = () => {
   const [cocktail, setCocktail] = useState<CocktailData | null>(null)
   const [showModalRecipe, setShowModalRecipe] = useState(false)
-  const [showModalWeather, setShowModalWeather] = useState(false)
+  // const [showModalWeather, setShowModalWeather] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
-  // const [displayedLocation, setDisplayedLocation] = useState('')
-  // const [weatherInfo, setWeatherInfo] = useState<WeatherData | null>(null)
-  // const [weatherImage, setWeatherImage] = useState<string | null>(null)
-  const [weatherData, setWeatherData] = useState<{
-    info: WeatherData | null
-    image: string | null
-  }>({
-    info: null,
-    image: null,
-  })
+  // const [weatherData, setWeatherData] = useState<{
+  //   info: WeatherData | null
+  //   image: string | null
+  // }>({
+  //   info: null,
+  //   image: null,
+  // })
   const router = useRouter()
-  const location = router.query.location as string
+  //const location = router.query.location as string
+  // const [location, setLocation] = useState('');
 
-  const updateWeatherData = (
-    weatherInfo: WeatherData | null,
-    weatherImage: string | null,
-  ) => {
-    setWeatherData({ info: weatherInfo, image: weatherImage })
-  }
+  // const updateWeatherData = (
+  //   weatherInfo: WeatherData | null,
+  //   weatherImage: string | null,
+  // ) => {
+  //   setWeatherData({ info: weatherInfo, image: weatherImage })
+  // }
 
-  useEffect(() => {
-    fetch('/api/cocktail1')
-      .then((response) => response.json())
-      .then((data: CocktailData) => setCocktail(data))
-      .catch((error) => console.error('Error fetching data:', error))
-  }, [])
+  // useEffect(() => {
+  //   const queryLocation = router.query.location as string
+  //   if (queryLocation && queryLocation.trim() !== '' && !cocktail) {
+  //           fetch('/api/cocktail1')
+  //       .then((response) => response.json())
+  //       .then((data: CocktailData) => setCocktail(data))
+  //       .catch((error) => setError('Error fetching data'))
+  //       .finally(() => setIsLoading(false))
+  //   }
+  // }, [router.query.location, cocktail, router]);
 
-  const fetchWeatherInfoFromServer = async (
-    requestedLocation: string,
-  ): Promise<void> => {
-    try {
-      // if (!location || (weatherInfo && weatherImage)) {
-      //   // If weather information and image already exist, do not fetch again
-      //   return;
-      // }
-      // setIsLoading(true);
-      if (!requestedLocation) return
+  //   useEffect(() => {
+  //   const fetchWeatherInfoFromServer = async (
+  //     requestedLocation: string,
+  //   ): Promise<void> => {
+  //     try {
+  //       if (!requestedLocation) return
 
-      const resWeatherInfoFromServer = await fetch(
-        `/api/weather1?location=${requestedLocation}`,
-      )
+  //       const resWeatherInfoFromServer = await fetch(
+  //         `/api/weather1?location=${requestedLocation}`,
+  //       )
 
-      const dataFromWeatherAPI =
-        (await resWeatherInfoFromServer.json()) as WeatherDataResponse
-      if (dataFromWeatherAPI.weatherInfo && dataFromWeatherAPI.image) {
-        //  setWeatherInfo(dataFromWeatherAPI.weatherInfo);
-        //  setWeatherImage(dataFromWeatherAPI.image);
-        updateWeatherData(
-          dataFromWeatherAPI.weatherInfo,
-          dataFromWeatherAPI.image,
-        )
-        // const weatherImage = setWeatherImage(dataFromWeatherAPI.image)
-        // const weatherInfo = setWeatherInfo(dataFromWeatherAPI.weatherInfo)
-        // console.log(weatherImage)
-        // console.log(
-        //   'the weather info from dataFromWeatherAPI is: ',
-        //   weatherInfo,
-        // )
-      }
-    } catch (error) {
-      console.error(
-        'There has been a problem with your fetch operation:',
-        error,
-      )
+  //       const dataFromWeatherAPI =
+  //         (await resWeatherInfoFromServer.json()) as WeatherDataResponse
+  //       if (dataFromWeatherAPI.weatherInfo && dataFromWeatherAPI.image) {
+  //         updateWeatherData(
+  //           dataFromWeatherAPI.weatherInfo,
+  //           dataFromWeatherAPI.image,
+  //         )
+  //       }
+  //     } catch (error) {
+  //       console.error(
+  //         'There has been a problem with your fetch operation:',
+  //         error,
+  //       )
 
-      // Insert the error handling code here
-      if (typeof error === 'object' && error !== null && 'message' in error) {
-        setError((error as Error).message)
-      } else {
-        setError('An unknown error occurred')
-      }
-    }
-    setIsLoading(false)
-  }
+  //       // Insert the error handling code here
+  //       if (typeof error === 'object' && error !== null && 'message' in error) {
+  //         setError((error as Error).message)
+  //       } else {
+  //         setError('An unknown error occurred')
+  //       }
+  //     }
+  //     setIsLoading(false)
+  //   }
+  //   if (!location && router.query.location) {
+  //     const requestedLocation = router.query.location as string;
+  //     setLocation(requestedLocation);
+  //     void fetchWeatherInfoFromServer(requestedLocation);
+  //   }
+  // }, [location, router.query.location]);
 
   useEffect(() => {
-    if (location) {
-      void fetchWeatherInfoFromServer(location)
+    const { cocktailData } = router.query;
+    if (cocktailData) {
+      setCocktail(JSON.parse(cocktailData as string) as CocktailData);
     }
-  }, [location])
+  }, [router.query.cocktailData]);
 
   //======================>  Create Cocktail Modal <============================== //
   const CocktailMenuModal = () => {
@@ -246,104 +238,103 @@ const CityWeather = () => {
   }
 
   // ========================> Weather Modal <============================== //
-  const WeatherModal = () => {
-    // const handleBackButtonClick = () => {
-    //   setShowModalRecipe(false) // Close the modal when the "Back" button is clicked
-    // }
-    return (
-      <div>
-        <Transition
-          show={showModalWeather}
-          enter="transition-opacity duration-75"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-150"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Fragment>
-            <ModalWeather
-              isVisible={showModalWeather}
-              onClose={() => setShowModalWeather(false)}
-              // children={undefined}
-            >
-              <div className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-blue-500 bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 ">
-                <div className="flex flex-shrink-0 items-center justify-between rounded-t-md   border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50  ">
-                  {/* <!--Modal title--> */}
-                  <h5
-                    className="flex-1 items-center  text-center text-xl font-medium leading-normal text-primaryd dark:text-neutral-200"
-                    id="exampleModalLabel"
-                  >
-                 
-                 <h2> In {weatherData.info ? weatherData.info.name : 'unknown location'} now </h2> 
-                                  </h5>
-                  {/* <!--Close button--> */}
-                  <button
-                    type="button"
-                    className="box-content rounded-none border-none text-primaryd hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-                    data-te-modal-dismiss
-                    aria-label="Close"
-                    onClick={() => setShowModalWeather(false)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-6 w-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </div>
+  // const WeatherModal = () => {
+  //   // const handleBackButtonClick = () => {
+  //   //   setShowModalRecipe(false) // Close the modal when the "Back" button is clicked
+  //   // }
+  //   return (
+  //     <div>
+  //       <Transition
+  //         show={showModalWeather}
+  //         enter="transition-opacity duration-75"
+  //         enterFrom="opacity-0"
+  //         enterTo="opacity-100"
+  //         leave="transition-opacity duration-150"
+  //         leaveFrom="opacity-100"
+  //         leaveTo="opacity-0"
+  //       >
+  //         <Fragment>
+  //           <ModalWeather
+  //             isVisible={showModalWeather}
+  //             onClose={() => setShowModalWeather(false)}
+  //             // children={undefined}
+  //           >
+  //             <div className="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-blue-500 bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 ">
+  //               <div className="flex flex-shrink-0 items-center justify-between rounded-t-md   border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50  ">
+  //                 {/* <!--Modal title--> */}
+  //                 <h5
+  //                   className="flex-1 items-center  text-center text-xl font-medium leading-normal text-primaryd dark:text-neutral-200"
+  //                   id="exampleModalLabel"
+  //                 >
 
-                {/* <!--Modal body--> */}
-                <div
-                  className="relative flex justify-between items-center justify-center bg-black p-0 first-line:flex-row"
-                  data-te-modal-body-ref
-                >
-                  <div className="flex flex-row">
+  //                <h2> In {weatherData.info ? weatherData.info.name : 'unknown location'} now </h2>
+  //                                 </h5>
+  //                 {/* <!--Close button--> */}
+  //                 <button
+  //                   type="button"
+  //                   className="box-content rounded-none border-none text-primaryd hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+  //                   data-te-modal-dismiss
+  //                   aria-label="Close"
+  //                   onClick={() => setShowModalWeather(false)}
+  //                 >
+  //                   <svg
+  //                     xmlns="http://www.w3.org/2000/svg"
+  //                     fill="none"
+  //                     viewBox="0 0 24 24"
+  //                     stroke-width="1.5"
+  //                     stroke="currentColor"
+  //                     className="h-6 w-6"
+  //                   >
+  //                     <path
+  //                       stroke-linecap="round"
+  //                       stroke-linejoin="round"
+  //                       d="M6 18L18 6M6 6l12 12"
+  //                     />
+  //                   </svg>
+  //                 </button>
+  //               </div>
 
-                   
-                        <section className=" INGR mx-8 text-white">
-                          <strong>Ingredient 1:</strong>
-                          {weatherData.info && (
-                            <div>
-                              <h2> In {weatherData.info.name} now </h2>
-                              <h2>
-                                {weatherData.info.weather[0]?.description}
-                              </h2>
-                              <h2>
-                                temperature {weatherData.info.main.feels_like}
-                              </h2>
-                              <h2>
-                                visibility is {weatherData.info.visibility} m
-                              </h2>
-                              <h2>wind {weatherData.info.wind.speed} m/s</h2>
-                            </div>
-                          )}
-                        </section>
-              
-                  </div>
-                </div>
-                {/* <!--Modal footer--> */}
-                <div className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md   bg-blue-500 border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-                  {/* <ButtonBack onClick={handleBackButtonClick} />
-                  <ButtonNext href="/input/waiting/editing/common" /> */}
-                  <button> Close</button>
-                </div>
-              </div>
-            </ModalWeather>
-          </Fragment>
-        </Transition>
-      </div>
-    )
-  }
+  //               {/* <!--Modal body--> */}
+  //               <div
+  //                 className="relative flex justify-between items-center justify-center bg-black p-0 first-line:flex-row"
+  //                 data-te-modal-body-ref
+  //               >
+  //                 <div className="flex flex-row">
+
+  //                       <section className=" INGR mx-8 text-white">
+  //                         <strong>Ingredient 1:</strong>
+  //                         {weatherData.info && (
+  //                           <div>
+  //                             <h2> In {weatherData.info.name} now </h2>
+  //                             <h2>
+  //                               {weatherData.info.weather[0]?.description}
+  //                             </h2>
+  //                             <h2>
+  //                               temperature {weatherData.info.main.feels_like}
+  //                             </h2>
+  //                             <h2>
+  //                               visibility is {weatherData.info.visibility} m
+  //                             </h2>
+  //                             <h2>wind {weatherData.info.wind.speed} m/s</h2>
+  //                           </div>
+  //                         )}
+  //                       </section>
+
+  //                 </div>
+  //               </div>
+  //               {/* <!--Modal footer--> */}
+  //               <div className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md   bg-blue-500 border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+  //                 {/* <ButtonBack onClick={handleBackButtonClick} />
+  //                 <ButtonNext href="/input/waiting/editing/common" /> */}
+  //                 <button> Close</button>
+  //               </div>
+  //             </div>
+  //           </ModalWeather>
+  //         </Fragment>
+  //       </Transition>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div>
@@ -367,7 +358,7 @@ const CityWeather = () => {
           <section className="flex flex-col items-center justify-center py-2 bg-green-500">
             left
             <div className="m-5 rounded-3xl bg-gradient-to-t from-tertiaryd to-secondaryd p-1 shadow-xl ">
-              <div className="   rounded-3xl border-solid border-accentd ">
+              {/* <div className="   rounded-3xl border-solid border-accentd ">
                 <a
                   aria-current="page"
                   className="text m-0  flex h-[100px] w-48 items-center justify-center  rounded-3xl border-solid  border-accentd bg-primaryd p-4  text-accentd ring-2  ring-tertiaryd  hover:bg-[#D9E5E2] "
@@ -376,11 +367,11 @@ const CityWeather = () => {
                 >
                   <h3 className="text-2xl font-bold">Get Weather</h3>
                 </a>
-                {showModalWeather && <WeatherModal />}
-              </div>
+                // {showModalWeather && <WeatherModal />}
+              </div> */}
             </div>
           </section>
-          <section className="flex flex-col items-center justify-center py-2 bg-blue-500">
+          {/* <section className="flex flex-col items-center justify-center py-2 bg-blue-500">
             center
             {weatherData.image && (
               <Image
@@ -401,12 +392,12 @@ const CityWeather = () => {
                 </div>
               )}
             </div>
-          </section>
+          </section> */}
           <section className="flex flex-col items-center justify-center py-2 bg-yellow-500">
             right
             <div className="m-5 rounded-3xl bg-gradient-to-t from-tertiaryd to-secondaryd p-1 shadow-xl ">
-              <div className="   rounded-3xl border-solid border-accentd ">
-                <a
+              <div className="rounded-3xl border-solid border-accentd ">
+                {/* <a
                   aria-current="page"
                   className="text m-0  flex h-[100px] w-48 items-center justify-center  rounded-3xl border-solid  border-accentd bg-primaryd p-4  text-accentd ring-2  ring-tertiaryd  hover:bg-[#D9E5E2] "
                   href="#"
@@ -414,7 +405,7 @@ const CityWeather = () => {
                 >
                   <h3 className="text-2xl font-bold">Get Recipe</h3>
                 </a>
-                {showModalRecipe && <CocktailMenuModal />}
+                {showModalRecipe && <CocktailMenuModal />} */}
               </div>
             </div>
             {cocktail && (
